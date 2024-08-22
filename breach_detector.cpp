6ad164887121167b978633647148f3ea71254843
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-08-22 20:24:11 +0530

    Update breach_detector.cpp

--- breach_detector.cpp
+++ breach_detector.cpp
@@ -14,3 +14,4 @@ breach_type classify_temperature_breach(cooling_type type, double temperature) {
     cooling_config config = get_cooling_config(type);
     return infer_breach(temperature, config.lower_limit, config.upper_limit);
 }
+
