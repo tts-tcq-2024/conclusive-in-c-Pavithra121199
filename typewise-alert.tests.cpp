d3236ffe536c5e4edcc37491f8e78a713444ea7b
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-08-22 16:40:11 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,7 +1,51 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+TEST(TypeWiseAlertTestSuite, InfersBreachAccordingToLimits) {
+    EXPECT_EQ(inferBreach(12, 20, 30), TOO_LOW);
+    EXPECT_EQ(inferBreach(25, 20, 30), NORMAL);
+    EXPECT_EQ(inferBreach(35, 20, 30), TOO_HIGH);
+}
 
+TEST(TypeWiseAlertTestSuite, ClassifiesTemperatureBreachForCoolingTypes) {
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 36), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(MED_ACTIVE_COOLING, 41), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(PASSIVE_COOLING, 20), NORMAL);
+}
 
+TEST(TypeWiseAlertTestSuite, SendsAlertToController) {
+    testing::internal::CaptureStdout();
+    sendToController(TOO_HIGH);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "feed : 2\n");
+}
+
+TEST(TypeWiseAlertTestSuite, SendsAlertToEmail) {
+    testing::internal::CaptureStdout();
+    sendToEmail(TOO_LOW);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too low\n");
+
+    testing::internal::CaptureStdout();
+    sendToEmail(TOO_HIGH);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too high\n");
+}
+
+TEST(TypeWiseAlertTestSuite, CheckAndAlertToController) {
+    BatteryCharacter batteryChar = {PASSIVE_COOLING, "BrandX"};
+    
+    testing::internal::CaptureStdout();
+    checkAndAlert(TO_CONTROLLER, batteryChar, 36);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "feed : 2\n");
+}
+
+TEST(TypeWiseAlertTestSuite, CheckAndAlertToEmail) {
+    BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "BrandY"};    
+    testing::internal::CaptureStdout();
+    checkAndAlert(TO_EMAIL, batteryChar, 46);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "To: a.b@c.com\nHi, the temperature is too high\n");
 }
